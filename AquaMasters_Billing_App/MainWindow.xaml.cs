68ef522e7914373bb4d823b554eab6999f3a8b00
Cam Downs <1cysw0rdk0@gmail.com>
2018-06-10 23:47:06 -0400

    Basic class for a 'part' added. Explored File IO for saving parts

---
+++ AquaMasters_Billing_App/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+
+namespace AquaMasters_Billing_App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Write an object to a json formatted string in a file
+            Part json = new Part();
+
+            json.setCost(5.00);
+            json.setName("Shock");
+            json.setType("Chem");
+
+            string strPath = Environment.GetFolderPath(System.Environment.SpecialFolder.ApplicationData);
+            strPath = strPath + "\\Aquamasters\\test.ini";
+
+            string jsonData = JsonConvert.SerializeObject(json, Formatting.Indented);
+            System.IO.File.WriteAllText(strPath, jsonData);
+        }
+
+       
+    }
+
+    public class Part {
+
+        public string name;
+        public string type;
+        public decimal cost;
+
+        public decimal getCost() => this.cost;
+        public string  getName() => this.name;
+        public string  getType() => this.type;
+
+        public void setCost(double cost) => this.cost = Decimal.Parse(cost.ToString());
+        public void setName(string  name) => this.name = name;
+        public void setType(string  type) => this.type = type;
+
+
+        public Part(double cost, string name, string type)
+        {
+            this.cost = Decimal.Parse(cost.ToString());
+            this.name = name;
+            this.type = type;
+        }
+
+        public Part() { }
+    }
+}
