6e1c3e47e891599c4f0dca0755c8668015b6f77c
Cam Downs <1cysw0rdk0@gmail.com>
2018-06-18 21:13:09 -0400

    Parts and Labor displays (mostly) fucntional   - missing removal function
    Started work on Opening UI

--- AquaMasters_Billing_App/MainWindow.xaml.cs
+++ AquaMasters_Billing_App/MainWindow.xaml.cs
@@ -25,6 +25,8 @@ namespace AquaMasters_Billing_App
 
         public Dictionary<string, Part> PriceSheet;
         public string savePath;
+        public List<PurchaseSet> partsList;
+        public List<PurchaseSet> laborList;
 
         public MainWindow()
         {
@@ -36,52 +38,71 @@ namespace AquaMasters_Billing_App
 
         private void InitializeBackendData() {
 
-            //Generate save path for this computer
+            // Generate save path for this computer
             this.savePath = Environment.GetFolderPath(System.Environment.SpecialFolder.ApplicationData);
             this.savePath += "\\Aquamasters\\test.ini";
 
-            //Initialize file to read text
+            // Initialize file to read text
             StreamReader file = File.OpenText(this.savePath);
 
-            //Initialize price sheet with new empty dictionary
+            // Initialize price sheet with new empty dictionary
             this.PriceSheet = new Dictionary<string, Part>();
 
-            //Read every line from the file, desearializing into parts and adding to dictonary
+            // Read every line from the file, desearializing into parts and adding to dictonary
             while (!file.EndOfStream)
             {
                 Part part = (Part)JsonConvert.DeserializeObject(file.ReadLine(), typeof(Part));
                 this.PriceSheet.Add(part.name, part);
             }
 
+            // Create and Bind data lists
+            this.partsList = new List<PurchaseSet>();
+            this.PartsDG.ItemsSource = this.partsList;
 
-            
+            this.laborList = new List<PurchaseSet>();
+            this.LaborDG.ItemsSource = this.laborList;
 
         }
 
 
         private void InitializeFrontendData() {
 
+            // Initialize columns for the parts display
+            this.PartsDG.Columns.Add(new DataGridTextColumn { Header = "Quantity", Binding = new Binding("quantity"), Width=56 });
+            this.PartsDG.Columns.Add(new DataGridTextColumn { Header = "Name", Binding = new Binding("part.name"), Width=242});
+            this.PartsDG.Columns.Add(new DataGridTextColumn { Header = "Cost", Binding = new Binding("part.cost"), Width=50 });
+            this.PartsDG.Columns.Add(new DataGridCheckBoxColumn{ Header = " X", Width = 30, CanUserSort=false, IsReadOnly=false });
             
+            // Initialize columns for the labor display
+            this.LaborDG.Columns.Add(new DataGridTextColumn { Header = "Hours", Binding = new Binding("quantity"), Width = 42 });
+            this.LaborDG.Columns.Add(new DataGridTextColumn { Header = "Men", Binding = new Binding("part.name"), Width = 50 });
+            this.LaborDG.Columns.Add(new DataGridTextColumn { Header = "Rate", Binding = new Binding("part.cost"), Width = 52 });
+            this.LaborDG.Columns.Add(new DataGridCheckBoxColumn { Header = " X", Width = 30, CanUserSort=false, IsReadOnly=false })
 
         }
 
 
         private void UpdateTotals() {
+            
+            decimal runningTotal = 0;
+            foreach (PurchaseSet purchase in this.partsList) {
+                runningTotal += Decimal.Parse(purchase.part.cost) * purchase.quantity;
+            }
+            this.PartCostTB.Text = runningTotal.ToString("0.00");
 
-            this.SubtotalCostTB.Text = (Decimal.Parse(this.PartCostTB.Text) + Decimal.Parse(this.LaborCostTB.Text)).ToString();
-            string cost = (Decimal.Parse(this.SubtotalCostTB.Text) * 1.0635m).ToString();
-            this.TotalCostTB.Text = cost.Substring(0, cost.IndexOf(".") + 3);
+            runningTotal = 0;
+            foreach (PurchaseSet purchase in this.laborList) {
+                runningTotal += Decimal.Parse(purchase.part.cost) * purchase.quantity;
+            }
+            this.LaborCostTB.Text = runningTotal.ToString("0.00");
+
+            this.SubtotalCostTB.Text = (Decimal.Parse(this.PartCostTB.Text) + Decimal.Parse(this.LaborCostTB.Text)).ToString("0.00");
+            string cost = (Decimal.Parse(this.SubtotalCostTB.Text) * 1.0635m).ToString("0.00");
+            this.TotalCostTB.Text = cost;
 
         }
 
-
-
-
-
-
-
-
-
+        // Save to file example
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             //Write an object to a json formatted string in a file
@@ -98,43 +119,112 @@ namespace AquaMasters_Billing_App
             System.IO.File.WriteAllText(strPath, jsonData);
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void REMOVE_ME_Click(object sender, RoutedEventArgs e)
         {
-            UpdateTotals();
+
+            AddPart(new Part(10.00M, "test2", "Chem"), 1);
+            AddPart(new Part(10.00M, "test2", "Chem"), 5);
+            AddPart(new Part(10.50M, "test2", "Chem"), 1);
+            AddPart(new Part(10.00M, "test2", "Chem"), 1);
+            AddPart(new Part(10.00M, "test2", "Chem"), 1);
+            AddPart(new Part(10.00m, "test2", "Chem"), 1);
+            AddPart(new Part(10.00m, "test2", "Chem"), 1);
+            AddPart(new Part(10.00m, "test2", "Chem"), 1);
+            AddPart(new Part(10.00m, "test2", "Chem"), 1);
+            AddPart(new Part(149.00m, "testLabor", "Labor"), 1);
+
+
 
         }
 
+        private void AddPart(Part newPart, int quantity) {
+
+            PurchaseSet purchaseSet = new PurchaseSet { part = newPart, quantity = quantity };
+
+            if (purchaseSet.part.type.Equals("Labor")) {
+                this.laborList.Add(purchaseSet);
+            } else {
+                this.partsList.Add(purchaseSet);
+            }
+
+            this.PartsDG.Items.Refresh();
+            this.LaborDG.Items.Refresh();
+
+
+            
+
+            /**
+            // Uncomment this code block to condense all items of a single name into one block. otherwise, they will
+            // be added as separate items to allow for easier comparison to the actual billing chart. 
+            
+            if (this.PartsDG.Items.Contains(purchaseSet)) {
+
+                //Increase the quantity of this item should it already exist in the list.
+                int index = this.PartsDG.Items.IndexOf(purchaseSet);
+                PurchaseSet old = (PurchaseSet)this.PartsDG.Items.GetItemAt(index);
+                old.quantity += purchaseSet.quantity;
+
+                //Tempting... but that's bad style :'(
+                //((PurchaseSet)this.PartsDG.Items.GetItemAt(this.PartsDG.Items.IndexOf(purchaseSet))).quantity += purchaseSet.quantity;
+
+            }
+            */
+
+            UpdateTotals();
+        }
 
     }
+
+
+
+
+
 
     /**
      * Part: the basic framework for a part or chem
      *      has a name, type and cost.
      *      fields are public to allow JsonConvert
-     *      to serialize the object.
+     *      to serialize the object, and DataGrid to 
+     *      Autopopulate the row fields
      */
     public class Part {
 
-        public string name;
-        public string type;
-        public decimal cost;
+        public string name { set; get; }
+        public string type { set; get; }
+        public string cost { set; get; }
 
-        public decimal getCost() => this.cost;
+        public string getCost() => this.cost;
         public string  getName() => this.name;
         public string  getType() => this.type;
 
-        public void setCost(double cost) => this.cost = Decimal.Parse(cost.ToString());
+        public void setCost(double cost) => this.cost = cost.ToString();
         public void setName(string  name) => this.name = name;
         public void setType(string  type) => this.type = type;
 
 
-        public Part(double cost, string name, string type)
+        public Part(decimal cost, string name, string type)
         {
-            this.cost = Decimal.Parse(cost.ToString());
+            this.cost = cost.ToString("0.00");
             this.name = name;
             this.type = type;
         }
 
         public Part() { }
+    }
+
+    public class PurchaseSet {
+
+        public Part part { set; get; }
+        public int quantity { set; get; }
+
+        public override bool Equals(Object test) {
+
+            if (test == null || GetType() != test.GetType()) { return false; }
+
+            PurchaseSet purchase = (PurchaseSet)test;
+            return (this.part.name == purchase.part.name);
+        }
+
+        public override int GetHashCode() => base.GetHashCode();
     }
 }
---
+++ AquaMasters_Billing_App/addOpening.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AquaMasters_Billing_App
+{
+    /// <summary>
+    /// Interaction logic for addOpening.xaml
+    /// </summary>
+    public partial class addOpening : Window
+    {
+        public addOpening()
+        {
+            InitializeComponent();
+        }
+
+        private void AssembleOnlyCB_Checked(object sender, RoutedEventArgs e)
+        {
+            if (this.AssembleOnlyCB.IsChecked.Value) {
+
+            }
+        }
+
+        private void AssembleOnlyCB_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        private void UpdateVisibility() {
+
+            if (this.AssembleOnlyCB.IsChecked.Value) {
+                CleanCoverCB.IsChecked = false;
+                AddChemsCB.IsChecked = false;
+
+                this.CleanCoverCB.Opacity = 0;
+                this.AddChemsCB.Opacity = 0;
+
+            } else {
+                this.CleanCoverCB.Opacity = 100;
+                this.AddChemsCB.Opacity = 100;
+            }
+
+        }
+    }
+}
